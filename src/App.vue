96b656ffed4aa83cff67bd1344a04ba5b734a604
p8844 <957442653@qq.com>
2024-03-06 15:38:29 +0800

    3.3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="App">
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.App{
+  display: flex;
+  height: 100vh;
+  background-color: pink;
+}
+</style>
